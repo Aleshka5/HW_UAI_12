4730ce0c0b1d5648564a6f88c8f2ee2833b421a0
Aleshka5 <fad80223@gmail.com>
2022-10-31 21:29:19 +0300

    commit main code requests again

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ url_vacancies = f'{DOMAIN}vacancies'
 url_areas = 'https://api.hh.ru/areas/'
 dict_requirement = {}
 for page_num in range(10):
-    time.sleep(2)
+    time.sleep(1)
     params = {
         'text': 'python developer',
         'page': page_num,
@@ -29,7 +29,7 @@ for page_num in range(10):
             str_result = str_result.split(' ')
             for word in str_result:
                 if len(word) > 0:
-                    if (word.lower().find('а') == -1) and (word.lower().find('в') == -1)and (word.lower().find('о') == -1)and (word.lower().find('п') == -1)and (word.lower().find('е') == -1)and (word.lower().find('и') == -1)and (word.lower().find('с') == -1):
+                    if (word.lower().find('а') == -1) and (word.lower().find('в') == -1)and (word.lower().find('о') == -1)and (word.lower().find('п') == -1)and (word.lower().find('е') == -1)and (word.lower().find('и') == -1)and (word.lower().find('с') == -1)and (word.lower().find('д') == -1):
                         try:
                             dict_requirement[word.lower()] += 1
                         except:
@@ -38,5 +38,9 @@ for page_num in range(10):
             print(f'Index:{i}')
             break
 
+dict_requirement = sorted(dict_requirement.items(),key=lambda item: item[1],reverse=True)
 pprint.pprint(dict_requirement)
+with open('res.json','w') as f:
+    json.dump(dict_requirement,f)
+
 
